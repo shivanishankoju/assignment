0a996ec55fe9a52e19eb1870549ed1f530c30789
shivanishankoju <shankojus@gmail.com>
2026-03-25 19:40:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes');
 
+// middleware
 app.use(express.json());
-app.use('/', routes);
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
 });
 
+/* other API routes */
+app.get("/api/students", (req, res) => {
+  res.send("Student API working");
+});
+
+/* React build serving (if you added step 1) */
+const path = require("path");
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
